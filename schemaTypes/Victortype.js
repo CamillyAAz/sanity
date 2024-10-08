f7600015d54f44e4f9d6b9bfc402dbbcf778f144
victorschmidt21 <naoseivtx21@gmail.com>
2024-10-07 21:45:58 -0300

    Victorr

---
+++ schemaTypes/Victortype.js
@@ -0,0 +1,29 @@
+import {defineField, defineType} from 'sanity'
+
+export const VictorType = defineType({
+  name: 'event',
+  title: 'Cadastro',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'nome',
+      type: 'string',
+    }),
+    defineField({
+        name: 'sobrenome',
+        type: 'string',
+    }),
+    defineField({
+        name: 'Idade',
+        type: 'number', 
+    }),
+    defineField({
+        name: 'imagem',
+        type: 'image',
+    }),
+    defineField({
+        name: 'Viado',
+        type: 'boolean',
+    }),
+  ],
+})
--- schemaTypes/index.js
+++ schemaTypes/index.js
@@ -1,4 +1,5 @@
 import { eventoVitor } from "./eventVitor"
 import { testeType } from "./testeType"
+import { VictorType } from "./Victortype"
 
-export const schemaTypes = [testeType, eventoVitor]
+export const schemaTypes = [testeType, eventoVitor, VictorType]
